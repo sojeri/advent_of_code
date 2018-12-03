a22fafe4b1e72fcf0961fea11a5a1c0a2c81fbe7
Jeri Sommers 👾 <sojeri@uw.edu>
2018-12-03 03:04:07 -0800

    add day 3 solution

---
+++ 2018/03/03-1-solution.js
@@ -0,0 +1,51 @@
+const xyMarker = "@ ";
+const xyDivider = ",";
+const sizeMarker = ": ";
+const sizeDivider = "x";
+
+let grid;
+
+function findOverlappingClaims(string) {
+    // "#45 @ 711,780: 19x23"
+    let xyStart = string.indexOf(xyMarker) + xyMarker.length;
+    let xyEnd = string.indexOf(sizeMarker);
+    let sizeStart = xyEnd + sizeMarker.length;
+    let xy = string.slice(xyStart,xyEnd).split(xyDivider);
+    let xStart = Number(xy[0]);
+    let yStart = Number(xy[1]);
+    let size = string.slice(sizeStart).split(sizeDivider);
+    let xSize = Number(size[0]);
+    let ySize = Number(size[1]);
+
+    let newOverlappingClaims = 0;
+    for (let i = xStart; i < xStart + xSize; i++) {
+        if (!Array.isArray(grid[i])) {
+            grid[i] = [];
+        }
+
+        for (let j = yStart; j < yStart + ySize; j++) {
+            let currentVal = grid[i][j];
+            if (currentVal == 1) {
+                grid[i][j] = 2;
+                newOverlappingClaims++;
+            } else if (currentVal != 2) {
+                grid[i][j] = 1;
+            }
+        }
+    }
+
+    return newOverlappingClaims;
+}
+
+function countInvalidGridLocations(claims, size = 1000) {
+    grid = new Array(size);
+
+    let contestedSpace = 0;
+    claims.forEach(c => {
+        contestedSpace += findOverlappingClaims(c);
+    });
+    
+    return contestedSpace;
+}
+
+module.exports = countInvalidGridLocations;
---
+++ 2018/03/03-2.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+// let runCallbackAgainstFile = require('../utils/runCbAgainstFileAsArray');
+let solution = require('./03-2-solution');
+
+describe('03-2-solution', () => {
+    describe('solution', () => {
+        it('should return 3 for example input', () => {
+            let example = [
+                '#1 @ 1,3: 4x4',
+                '#2 @ 3,1: 4x4',
+                '#3 @ 5,5: 2x2'
+            ];
+            assert.equal(solution(example), '#3');
+        });
+    });
+});
