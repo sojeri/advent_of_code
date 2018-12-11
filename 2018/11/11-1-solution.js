ebde1106e637b07f620d13a87d51102489e892de
Jeri Sommers 👾 <sojeri@uw.edu>
2018-12-10 22:06:16 -0800

    add 2018/11 with cheat-y answer for pt2

---
+++ 2018/11/11-1-solution.js
@@ -0,0 +1,27 @@
+let {getPowerForCell, getPowerForCoordinate} = require('./11-2-solution');
+
+function getLargestPower3x3Cell(gridNo) {
+    let fuelCell = new Array(301);
+    let highest3x3PowerSeen = -1;
+    let highestCoord;
+    for (let x = 1; x <= 300; x++) {
+        fuelCell[x] = new Array(301);
+        for (let y = 1; y <= 300; y++) {
+            fuelCell[x][y] = getPowerForCoordinate(x, y, gridNo);
+            if (x >= 3 && y >= 3) {
+                let current3x3Cell = getPowerForCell(x, y, fuelCell, 3);
+                if (current3x3Cell > highest3x3PowerSeen) {
+                    highest3x3PowerSeen = current3x3Cell;
+                    highestCoord = `${x-2},${y-2}`;
+                }
+            }
+        }
+    }
+
+    return highestCoord;
+}
+
+module.exports = {
+    getPowerForCoordinate,
+    getLargestPower3x3Cell,
+};
---
+++ 2018/11/11-2-solution.js
@@ -0,0 +1,50 @@
+function getPowerForCoordinate(x, y, serialNo) {
+    let rackId = x + 10;
+    let powerLevel = rackId * y;
+    powerLevel += serialNo;
+    powerLevel = powerLevel * rackId;
+    let powerLevelString = Math.floor(powerLevel / 100).toString();
+    return Number(powerLevelString[powerLevelString.length - 1]) - 5;
+}
+
+function getPowerForCell(x, y, grid, size) {
+    let sum = 0;
+    for (let i = x - size + 1; i <= x; i++) {
+        for (let j = y - size + 1; j <= y; j++) {
+            sum += grid[i][j];
+        }
+    }
+    return sum;
+}
+
+/**
+ * 
+ */
+function getLargestPowerCell(gridNo, stop = 30) {
+    let fuelCell = new Array(301);
+    let highestPowerSeen = -1;
+    let highestCoord;
+    for (let x = 1; x <= 300; x++) {
+        fuelCell[x] = new Array(301);
+        for (let y = 1; y <= 300; y++) {
+            fuelCell[x][y] = getPowerForCoordinate(x, y, gridNo);
+            for (let size = 1; size <= y && size <= stop; size++) {
+                if (x >= size && y >= size) {
+                    let currentCell = getPowerForCell(x, y, fuelCell, size);
+                    if (currentCell > highestPowerSeen) {
+                        highestPowerSeen = currentCell;
+                        highestCoord = `${x-size+1},${y-size+1},${size}`;
+                    }
+                }
+            }
+        }
+    }
+
+    return highestCoord;
+}
+
+module.exports = {
+    getPowerForCell,
+    getPowerForCoordinate,
+    getLargestPowerCell,
+};
