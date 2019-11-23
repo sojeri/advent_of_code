c0751d0600a252ad34ee6a0e1542b1c591b615dd
Jeri Sommers 👾 <sojeri@uw.edu>
2019-11-22 22:21:55 -0800

    countTruths should handle values which are already true :)

--- utils/js/countTruths.js
+++ utils/js/countTruths.js
@@ -1,12 +1,14 @@
 const ArgumentErrorNotArray = 'ArgumentError: arrayOfPossibleTruths should be an array'
 const ArgumentErrorNotFunction = 'ArgumentError: truthinessCallback should be a function'
 
+doNothingCallback = (v) => { return v }
+
 /**
  * given an arrary of possible truths and a truthiness callback, returns the total count of possible truths according to said truthiness callback
  * @param {*} arrayOfPossibleTruths a collection of items to be verified by the truthiness callback
  * @param {*} truthinessCallback a function which returns a boolean value indicating whether an item is truthy
  */
-function countTruths(arrayOfPossibleTruths, truthinessCallback) {
+function countTruths(arrayOfPossibleTruths, truthinessCallback=doNothingCallback) {
     if (!Array.isArray(arrayOfPossibleTruths)) {
         throw new Error(ArgumentErrorNotArray)
     }
