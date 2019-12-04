d9baded2166177cedcd58a61a4d008efb6df42a7
Jeri Sommers 👾 <sojeri@uw.edu>
2019-12-03 18:11:55 -0800

    add comments I was too tired to add last night :)

--- 2019/03/03-1-solution.js
+++ 2019/03/03-1-solution.js
@@ -1,51 +1,75 @@
+/**
+ * keeps track of already seen coordinates, eg {'0,1': true}.
+ * if one is ever seen twice, we have a wire intersection.
+ */
 let seen
-let knownIntersections
-let currentLocation
 
-function moveBlocks(axis, modifier, stop, name) {
-    for (let start = 1; start <= stop; start++) {
-        currentLocation[axis] = currentLocation[axis] + modifier
-        let coord = `${currentLocation.x},${currentLocation.y}`
+/**
+ * keeps track of intersection manhattan distances.
+ * (every time a new intersection is found, its manhattan distance is saved here.)
+ */
+let knownIntersections
+
+/**
+ * tracks a given wire's movement along a given axis for a given number of steps
+ * @param {*} axis the axis (x or y) to track movement along
+ * @param {*} modifier a numeric value (1 or -1) indicating whether movement is up or down the given axis
+ * @param {*} steps how many steps to take along the axis
+ * @param {*} wire the wire to track movement for signature: { x, y, name }
+ */
+function trackMovement(axis, modifier, steps, wire) {
+    for (let s = 1; s <= steps; s++) {
+        wire[axis] = wire[axis] + modifier
+        let coord = `${wire.x},${wire.y}`
 
         if (!seen[coord]) {
             seen[coord] = []
         }
 
-        if (seen[coord].indexOf(name) == -1) {
-            seen[coord].push(name)
+        if (seen[coord].indexOf(wire.name) == -1) {
+            seen[coord].push(wire.name)
 
             if (seen[coord].length == 2) {
-                knownIntersections.push(Math.abs(currentLocation.x) + Math.abs(currentLocation.y))
+                knownIntersections.push(Math.abs(wire.x) + Math.abs(wire.y))
             }
         }
     }
 }
 
+/**
+ * for a given collection of wires, returns the manhattan distance of the wire intersection closest to the origin (0,0).
+ * full spec: https://adventofcode.com/2019/day/3
+ * @param {*} wires a set of wires as defined by their movements (eg, R6,U34,L7)
+ */
 function findClosestIntersection(wires) {
     seen = {}
     knownIntersections = []
 
+    // track all the wires
     wires.forEach((wireInstructions, wireName) => {
-        currentLocation = { x: 0, y: 0 }
+        let wire = {
+            name: wireName,
+            x: 0,
+            y: 0,
+        }
 
         let instructions = wireInstructions.split(',')
         for (let i = 0; i < instructions.length; i++) {
-            let instruction = instructions[i]
+            let instruction = instructions[i] // eg, R76
 
-            // handle move
-            let move = Number(instruction.slice(1))
+            let howFar = Number(instruction.slice(1)) // eg, 76
             switch (instruction[0]) {
                 case 'U':
-                    moveBlocks('y', 1, move, wireName)
+                    trackMovement('y', 1, howFar, wire)
                     break
                 case 'D':
-                    moveBlocks('y', -1, move, wireName)
+                    trackMovement('y', -1, howFar, wire)
                     break
                 case 'R':
-                    moveBlocks('x', 1, move, wireName)
+                    trackMovement('x', 1, howFar, wire)
                     break
                 case 'L':
-                    moveBlocks('x', -1, move, wireName)
+                    trackMovement('x', -1, howFar, wire)
                     break
                 default:
                     throw new Error('unrecognized drection')
@@ -53,6 +77,7 @@ function findClosestIntersection(wires) {
         }
     })
 
+    // find shortest distance
     let min = knownIntersections[0]
     knownIntersections.forEach(k => {
         min = min < k ? min : k
--- 2019/03/03-2-solution.js
+++ 2019/03/03-2-solution.js
@@ -1,28 +1,42 @@
-let { ascendingSort } = require('../../utils/js/sorts')
+const { ascendingSort } = require('../../utils/js/sorts')
 
+/**
+ * keeps track of already seen coordinates, eg {'0,1': true}.
+ * if one is ever seen twice, we have a wire intersection.
+ */
 let seen
-let knownIntersections
-let currentLocation
-let steps
 
-function moveBlocks(axis, modifier, stop, name) {
+/**
+ * keeps track of known intersections.
+ *
+ */
+let knownIntersections
+
+/**
+ * tracks a given wire's movement along a given axis for a given number of steps
+ * @param {*} axis the axis (x or y) to track movement along
+ * @param {*} modifier a numeric value (1 or -1) indicating whether movement is up or down the given axis
+ * @param {*} steps how many steps to take along the axis
+ * @param {*} wire the wire to track movement for signature: { x, y, name, steps }
+ */
+function trackMovement(axis, modifier, stop, wire) {
     for (let start = 1; start <= stop; start++) {
-        steps++
-        currentLocation[axis] = currentLocation[axis] + modifier
-        let coord = `${currentLocation.x},${currentLocation.y}`
+        wire.steps++
+        wire[axis] = wire[axis] + modifier
+        let coord = `${wire.x},${wire.y}`
 
         if (!seen[coord]) {
             seen[coord] = {}
         }
 
-        if (!seen[coord][name]) {
-            seen[coord][name] = steps
+        if (!seen[coord][wire.name]) {
+            seen[coord][wire.name] = wire.steps
         }
 
         let here = Object.keys(seen[coord])
         if (here.length >= 2) {
             if (knownIntersections[coord]) {
-                knownIntersections[coord].push(steps)
+                knownIntersections[coord].push(wire.steps)
             } else {
                 knownIntersections[coord] = []
                 knownIntersections[coord].push(seen[coord][here[0]])
@@ -32,32 +46,40 @@ function moveBlocks(axis, modifier, stop, name) {
     }
 }
 
+/**
+ * for a given collection of wires, returns the least number of steps to the wire intersection closest to the origin (0,0).
+ * full spec: https://adventofcode.com/2019/day/3#part2
+ * @param {*} wires a set of wires as defined by their movements (eg, R6,U34,L7)
+ */
 function findShortestDistance(wires) {
     seen = {}
     knownIntersections = {}
 
     wires.forEach((wireInstructions, wireName) => {
-        steps = 0
-        currentLocation = { x: 0, y: 0 }
+        let wire = {
+            name: wireName,
+            x: 0,
+            y: 0,
+            steps: 0,
+        }
 
         let instructions = wireInstructions.split(',')
         for (let i = 0; i < instructions.length; i++) {
             let instruction = instructions[i]
 
-            // handle move
-            let move = Number(instruction.slice(1))
+            let howFar = Number(instruction.slice(1))
             switch (instruction[0]) {
                 case 'U':
-                    moveBlocks('y', 1, move, wireName)
+                    trackMovement('y', 1, howFar, wire)
                     break
                 case 'D':
-                    moveBlocks('y', -1, move, wireName)
+                    trackMovement('y', -1, howFar, wire)
                     break
                 case 'R':
-                    moveBlocks('x', 1, move, wireName)
+                    trackMovement('x', 1, howFar, wire)
                     break
                 case 'L':
-                    moveBlocks('x', -1, move, wireName)
+                    trackMovement('x', -1, howFar, wire)
                     break
                 default:
                     throw new Error('unrecognized drection')
@@ -75,7 +97,6 @@ function findShortestDistance(wires) {
 function findMinSteps(intersectionDetails) {
     let minSteps = []
 
-    // intersectionDetails[coord][someSteps,otherSteps,evenMoarSteps]
     Object.keys(intersectionDetails).forEach(intersection => {
         let stepCounts = intersectionDetails[intersection]
         let sorted = stepCounts.sort(ascendingSort)
