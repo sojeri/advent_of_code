98a0f9e6c98301fb3c9524365aad8285a6c74e84
Jeri Sommers 👾 <sojeri@uw.edu>
2020-12-03 22:15:24 -0800

    tweak test template a bit

--- utils/js/templates/test.js
+++ utils/js/templates/test.js
@@ -1,16 +1,16 @@
 const assert = require('assert')
-// let runCallbackAgainstFile = require('../../utils/js/runCbAgainstFileAsArray')
+let runCallbackAgainstFile = require('../../utils/js/runCbAgainstFileAsArray')
 let solution = require('./@@solution')
 
-describe('@@solution', () => {
+describe.only('@@solution', () => {
     describe('solution', () => {
-        it('should return -1 for example input', () => {
-            assert.strictEqual(solution(), -1)
-        })
         // it('should return -1 for example input', () => {
-        //     assert.strictEqual(runCallbackAgainstFile(solution, '##/example.txt'), -1)
+        //     assert.strictEqual(solution(), -1)
         // })
-        // it('should return -1 for example input', () => {
+        it('should return -1 for example input', () => {
+            assert.strictEqual(runCallbackAgainstFile(solution, '##/example.txt'), -1)
+        })
+        // it('should return -1 for puzzle input', () => {
         //     assert.strictEqual(runCallbackAgainstFile(solution, '##/input.txt'), -1)
         // })
     })
