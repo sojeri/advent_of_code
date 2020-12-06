87cb9194ac56a0155840e56d7d59867780397b90
Jeri Sommers 👾 <sojeri@uw.edu>
2020-12-05 21:19:16 -0800

    remove dangling .only

--- 2020/05-binary-boarding/05-2-test.js
+++ 2020/05-binary-boarding/05-2-test.js
@@ -2,7 +2,7 @@ const assert = require('assert')
 let runCallbackAgainstFile = require('../../utils/js/runCbAgainstFileAsArray')
 let solution = require('./05-2-solution')
 
-describe.only('05-2-solution', () => {
+describe('05-2-solution', () => {
     describe('solution', () => {
         it('should return 711 for puzzle input', () => {
             assert.strictEqual(runCallbackAgainstFile(solution, '2020/05-binary-boarding/input.txt'), 711)
