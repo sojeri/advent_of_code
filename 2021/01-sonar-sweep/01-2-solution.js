aac9ff1ce124540556c187294519b3748e2be0e2
Jeri Sommers 👾 <sojeri@uw.edu>
2021-12-05 02:44:44 -0800

    finish day 1

--- 2021/01-sonar-sweep/01-2-solution.js
+++ 2021/01-sonar-sweep/01-2-solution.js
@@ -1,7 +1,17 @@
-//
+// https://adventofcode.com/2021/day/1#part2
 
-function solution() {
-    // TODO: implement 01-2-solution
+function solution(input) {
+    const numero = index => Number(input[index])
+
+    let isBiggerCount = 0
+
+    for (let i = 0; i < input.length - 3; i++) {
+        const first = numero(i) + numero(i + 1) + numero(i + 2)
+        const second = first - numero(i) + numero(i + 3)
+        if (first < second) isBiggerCount++
+    }
+
+    return isBiggerCount
 }
 
 module.exports = solution
--- 2021/01-sonar-sweep/01-2-test.js
+++ 2021/01-sonar-sweep/01-2-test.js
@@ -2,16 +2,16 @@ const assert = require('assert')
 let runCallbackAgainstFile = require('../../utils/js/runCbAgainstFileAsArray')
 let solution = require('./01-2-solution')
 
-describe.only('01-2-solution', () => {
+describe('01-2-solution', () => {
     describe('solution', () => {
         // it('should return -1 for example input', () => {
         //     assert.strictEqual(solution(), -1)
         // })
-        it('should return -1 for example input', () => {
-            assert.strictEqual(runCallbackAgainstFile(solution, '2021/01-sonar-sweep/example.txt'), -1)
+        it('should return 5 for example input', () => {
+            assert.strictEqual(runCallbackAgainstFile(solution, '2021/01-sonar-sweep/example.txt'), 5)
         })
-        // it('should return -1 for puzzle input', () => {
-        //     assert.strictEqual(runCallbackAgainstFile(solution, '2021/01-sonar-sweep/input.txt'), -1)
-        // })
+        it('should return 1743 for puzzle input', () => {
+            assert.strictEqual(runCallbackAgainstFile(solution, '2021/01-sonar-sweep/input.txt'), 1743)
+        })
     })
 })
