671500c0522b2674056e1d3efa6658cc0631c2cd
Jeri Sommers 👾 <sojeri@uw.edu>
2021-12-07 23:20:04 -0800

    add a few reminders for my future self 😈

---
+++ utils/js/todos.js
@@ -0,0 +1,4 @@
+// TODO: script adding readme to day dir linking back to relevant AOC puzzle
+// TODO: add ^^ to scaffolding code
+// TODO: script clean/renames for when separate files not needed to solve
+// FIXME: delete unused scaffolds!
